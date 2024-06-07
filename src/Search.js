e110f9c5ee37036946ef42bb340c578e7485f64f
grumpyalpaca <119808526+grumpyalpaca@users.noreply.github.com>
2024-06-07 21:34:25 +0200

    had to fix the spaces in the footer bc they weren't being properly recognised

--- src/Search.js
+++ src/Search.js
@@ -25,31 +25,31 @@ export default function Search() {
         {submittedCity && <Results city={submittedCity} />}
       </div>
       <footer>
-        This project was coded by 
-        <a
-          href="https://github.com/grumpyalpaca"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Elly
-        </a>
-        , code hosted on 
-        <a
-          href="https://github.com/grumpyalpaca/hw3-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-        and website hosted on 
-        <a
-          href="https://main--stirring-pothos-4bbbe8.netlify.app//"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Netlify
-        </a>
-      </footer>
+  This project was coded by{" "}
+  <a
+    href="https://github.com/grumpyalpaca"
+    target="_blank"
+    rel="noreferrer"
+  >
+    Elly
+  </a>
+  , code hosted on{" "}
+  <a
+    href="https://github.com/grumpyalpaca/hw3-react"
+    target="_blank"
+    rel="noreferrer"
+  >
+    Github
+  </a>
+  {" "}and website hosted on{" "}
+  <a
+    href="https://main--stirring-pothos-4bbbe8.netlify.app//"
+    target="_blank"
+    rel="noreferrer"
+  >
+    Netlify
+  </a>
+</footer>
     </div>
   );
 }
