3ba38ce680edf683ffbcdc4c4be6d32c8ae46563
grumpyalpaca <119808526+grumpyalpaca@users.noreply.github.com>
2024-06-07 21:21:17 +0200

    Added footer of developer (me)

--- src/Search.js
+++ src/Search.js
@@ -16,12 +16,40 @@ export default function Search() {
 
   return (
     <div>
-      <form onSubmit={handleCity}>
-        <input type="search" placeholder="Search City" onChange={updateCity} />
-        <input type="submit" value="Search" />
-      </form>
-      <br />
-      {submittedCity && <Results city={submittedCity} />}
+      <div>
+        <form onSubmit={handleCity}>
+          <input type="search" placeholder="Search City" onChange={updateCity} />
+          <input type="submit" value="Search" />
+        </form>
+        <br />
+        {submittedCity && <Results city={submittedCity} />}
+      </div>
+      <footer>
+        This project was coded by 
+        <a
+          href="https://github.com/grumpyalpaca"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Elly
+        </a>
+        , code hosted on 
+        <a
+          href="https://github.com/grumpyalpaca/hw3-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+        and website hosted on 
+        <a
+          href="https://main--stirring-pothos-4bbbe8.netlify.app//"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
